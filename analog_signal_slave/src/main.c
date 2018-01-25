42f7cd14b09af7e32470f065dcd809676e4a77e9
Massaki Archambault <archambault.massaki@gmail.com>
2018-01-25 16:20:37 -0500

    flashing led at the same time than usart

--- analog_signal_slave/src/main.c
+++ analog_signal_slave/src/main.c
@@ -14,8 +14,29 @@
 #include <asf.h>
 #include "compiler.h"    // Definitions utile: de U8, S8, U16, S16, U32, S32, U32, U62, F32
 
+#define TC_LED_CHANNEL      0
+#define TC_LED              (&AVR32_TC)
+#define TC_LED_IRQ          AVR32_TC_IRQ0
+#define LED_INTRPT_PRIO     AVR32_INTC_INT0    // LED interrupt priority
+
+#define USART_INTRPT_PRIO   AVR32_INTC_INT1    // USART interrupt priority
+
+#define FPBA                FOSC0              // a 12Mhz
+#define FALSE               0
+
 U32 char_recu;       // Variables globales unsigned 32bits
 U8 compteur;         // Variables globales unsigned 8bits
+
+__attribute__((__interrupt__))
+static void led_interrupt_handler(void)
+{
+	// Reset l'interrupt?
+	tc_read_sr(TC_LED, TC_LED_CHANNEL);
+	//TODO conditionally toggle GPIO
+	gpio_tgl_gpio_pin(LED0_GPIO);
+	gpio_tgl_gpio_pin(LED1_GPIO);
+}
+
 //==================================================================================
 // USART interrupt handler.
 //  2 sources peuvent lancer cette interruption
@@ -67,6 +88,51 @@ static void usart_int_handler(void)
 //==================================================================================
 int main(void)
 {
+	U32 i;
+	
+	// Necessaire a la config des leds
+	volatile avr32_tc_t *tc_led = TC_LED;
+	static const tc_waveform_opt_t tc_led_waveform_opt =
+	{
+		.channel  = TC_LED_CHANNEL,                    // Channel selection.
+
+		.bswtrg   = TC_EVT_EFFECT_NOOP,                // Software trigger effect on TIOB.
+		.beevt    = TC_EVT_EFFECT_NOOP,                // External event effect on TIOB.
+		.bcpc     = TC_EVT_EFFECT_NOOP,                // RC compare effect on TIOB.
+		.bcpb     = TC_EVT_EFFECT_NOOP,                // RB compare effect on TIOB.
+
+		.aswtrg   = TC_EVT_EFFECT_NOOP,                // Software trigger effect on TIOA.
+		.aeevt    = TC_EVT_EFFECT_NOOP,                // External event effect on TIOA.
+		.acpc     = TC_EVT_EFFECT_NOOP,                // RC compare effect on TIOA: toggle.
+		.acpa     = TC_EVT_EFFECT_NOOP,                // RA compare effect on TIOA: toggle
+		.wavsel   = TC_WAVEFORM_SEL_UP_MODE_RC_TRIGGER,// Waveform selection: Up mode with automatic trigger(reset) on RC compare.
+		.enetrg   = FALSE,                             // External event trigger enable.
+		.eevt     = 0,                                 // External event selection.
+		.eevtedg  = TC_SEL_NO_EDGE,                    // External event edge selection.
+		.cpcdis   = FALSE,                             // Counter disable when RC compare.
+		.cpcstop  = FALSE,                             // Counter clock stopped with RC compare.
+
+		.burst    = FALSE,                             // Burst signal selection.
+		.clki     = FALSE,                             // Clock inversion.
+		.tcclks   = TC_CLOCK_SOURCE_TC4                // Internal source clock 3, connected to fPBA / 8.
+	};
+	static const tc_interrupt_t tc_led_interrupt_config =
+	{
+		.etrgs = 0,
+		.ldrbs = 0,
+		.ldras = 0,
+		.cpcs  = 1,
+		.cpbs  = 0,
+		.cpas  = 0,
+		.lovrs = 0,
+		.covfs = 0
+	};
+
+
+	/*******************************************/
+	/** Initialisation des variables globales **/
+	/*******************************************/
+	
 	static const gpio_map_t USART_GPIO_MAP =
 	{
 		{AVR32_USART1_RXD_0_0_PIN, AVR32_USART1_RXD_0_0_FUNCTION},
@@ -74,6 +140,35 @@ int main(void)
 	};
 	// initialisation des variables globales;
 	compteur=0;
+	
+	// Preparatif pour l'enregistrement des interrupt handler du INTC.
+	INTC_init_interrupts();
+	
+	// Desactive les interruptions pendant la configuration.
+	Disable_global_interrupt();
+	
+	/***************************/
+	/** Configuration des LED **/
+	/***************************/
+	
+	// On initialise le crystal externe sur le channel 0
+	pcl_switch_to_osc(PCL_OSC0, FOSC0, OSC0_STARTUP);
+	// On setup le interrupt handler
+	INTC_register_interrupt(&led_interrupt_handler, TC_LED_IRQ, LED_INTRPT_PRIO);
+	// Autoriser les interruptions.
+	Enable_global_interrupt();
+	// Initialise le timer
+	tc_init_waveform(tc_led, &tc_led_waveform_opt);
+	// Configure la fréquence du timer
+	// 1 interruption tous les .1s TODO: wtf this calculation means?
+	tc_write_rc(tc_led, TC_LED_CHANNEL, (FPBA /32 ) / 10);
+	tc_configure_interrupts(tc_led, TC_LED_CHANNEL, &tc_led_interrupt_config);
+	// Finalement, on demarre le timer
+	tc_start(tc_led, TC_LED_CHANNEL);
+	
+	/****************************/
+	/**	Configuration de USART **/
+	/****************************/
 
 	// Au boot, 115kHz, on doit passer au crystal FOSC0=12MHz avec le PM
 	pm_switch_to_osc0(&AVR32_PM, FOSC0, OSC0_STARTUP);
@@ -84,25 +179,19 @@ int main(void)
 	// Initialise le USART1 en mode seriel RS232, a 57600 BAUDS, a FOSC0=12MHz.
 	init_dbg_rs232(FOSC0);
 
-	// Desactive les interruptions pendant la configuration.
-	Disable_global_interrupt();
 
-	// Preparatif pour l'enregistrement des interrupt handler du INTC.
-	INTC_init_interrupts();
+	// Enregister le USART interrupt handler au INTC, level INT1
+	INTC_register_interrupt(&usart_int_handler, AVR32_USART1_IRQ, USART_INTRPT_PRIO);
 
-	// Enregister le USART interrupt handler au INTC, level INT0
-	INTC_register_interrupt(&usart_int_handler, AVR32_USART1_IRQ, AVR32_INTC_INT0);
-
-	print_dbg("Taper un caractere sur le clavier du PC avec TeraTerminal...\n\n");
+	print_dbg("Taper un caractere sur le clavier du PC avec TeraTerminal...\r\n\n");
 
 	// Activer la source d'interrution du UART en reception (RXRDY)
 	AVR32_USART1.ier = AVR32_USART_IER_RXRDY_MASK;
 
-	// Autoriser les interruptions.
-	Enable_global_interrupt();
 
 	while(1)
 	{
 		// Rien a faire, tout ce fait par interuption !
+		i++;
 	}
 }
